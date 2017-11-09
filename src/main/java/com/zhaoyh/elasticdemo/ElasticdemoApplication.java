703a4cfc470cbee594c3822879330f996ac31d4e
chadwick521 <yonghui_chad@163.com>
2017-11-09 14:32:56 +0800

    ADD

---
+++ src/main/java/com/zhaoyh/elasticdemo/ElasticdemoApplication.java
@@ -0,0 +1,12 @@
+package com.zhaoyh.elasticdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticdemoApplication.class, args);
+	}
+}
